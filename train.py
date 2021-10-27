d659ffcad4b1f2bf9c5cebe1bc24c47e0c068d74
XUE-gy <1039332103@qq.com>
2021-10-28 01:24:30 +0800

    .

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ import torch
 from torch.utils.data import DataLoader
 from torch import nn
 import argparse
-from tensorboardX import SummaryWriter
+# from tensorboardX import SummaryWriter
 
 from data_preparation.data_preparation import FileDateset
 from model.Baseline import Base_model
@@ -15,11 +15,11 @@ from model.ops import pytorch_LSD
 def parse_ags1():
     parser = argparse.ArgumentParser()
     # 重头开始训练 defaule=None, 继续训练defaule设置为'/**.pth'
-    parser.add_argument("--model_name", type=str, default=None, help="是否加载模型继续训练 '/50.pth' None")
-    parser.add_argument("--batch-size", type=int, default=16, help="")
-    parser.add_argument("--epochs", type=int, default=30, help='20')
-    parser.add_argument('--lr', type=float, default=3e-4, help='学习率 (default: 0.01,3e-4)')
-    parser.add_argument('--train_data', default="./data_preparation/Synthetic/VAL3", help='数据集的path')
+    parser.add_argument("--model_name", type=str, default='/670.pth', help="是否加载模型继续训练  None")
+    parser.add_argument("--batch-size", type=int, default=2, help="")
+    parser.add_argument("--epochs", type=int, default=1000, help='20')
+    parser.add_argument('--lr', type=float, default=1e-5, help='学习率 (default: 0.01,3e-4)')
+    parser.add_argument('--train_data', default="./data_preparation/Synthetic/TRAIN3", help='数据集的path')
     parser.add_argument('--val_data', default="./data_preparation/Synthetic/VAL3", help='验证样本的path')
     parser.add_argument('--checkpoints_dir', default="./checkpoints/AEC_baseline", help='模型检查点文件的路径(以继续培训)')
     parser.add_argument('--event_dir', default="./event_file/AEC_baseline", help='tensorboard事件文件的地址')
@@ -31,7 +31,7 @@ def main():
     args = parse_ags1()
     # args = parser.parse_args(args=[])
     print("GPU是否可用：", torch.cuda.is_available())  # True
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     # 实例化 Dataset
     train_set = FileDateset(dataset_path=args.train_data)  # 实例化训练数据集
@@ -62,11 +62,13 @@ def main():
     # lr_schedule = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[10,20], gamma=0.1)
 
     # ###########    TensorBoard可视化 summary  ############
-    writer = SummaryWriter(args.event_dir)  # 创建事件文件
+    # writer = SummaryWriter(args.event_dir)  # 创建事件文件
 
     # ###########    加载模型检查点   ############
     start_epoch = 0
-    if args.model_name:
+    isLoad = True
+    # if args.model_name:
+    if isLoad:
         print("加载模型：", args.checkpoints_dir + args.model_name)
         checkpoint = torch.load(args.checkpoints_dir + args.model_name)
         model.load_state_dict(checkpoint["model"])
@@ -85,28 +87,28 @@ def main():
             train_nearend_magnitude = train_nearend_magnitude.to(device)
 
             # 前向传播
-            pred_mask = model(train_X)  # [batch_size, 322, 999]--> [batch_size, 161, 999]
+            pred_mask = model(train_X).to(device)  # [batch_size, 322, 999]--> [batch_size, 161, 999]
             # train_loss = criterion(pred_mask, train_mask)
 
             # 近端语音信号频谱 = mask * 麦克风信号频谱 [batch_size, 161, 999]
-            pred_near_spectrum = pred_mask * train_nearend_mic_magnitude
-            train_lsd = pytorch_LSD(train_nearend_magnitude, pred_near_spectrum)
-            train_loss = criterion(train_nearend_magnitude, pred_near_spectrum)
-            print('train_loss:',train_loss)
+            pred_near_spectrum = (pred_mask * train_nearend_mic_magnitude).to(device)
+            train_lsd = pytorch_LSD(train_nearend_magnitude, pred_near_spectrum).to(device)
+            train_loss = criterion(train_nearend_magnitude, pred_near_spectrum).to(device)
+            # print('train_loss:',train_loss)
             # 反向传播
             optimizer.zero_grad()  # 将梯度清零
             train_loss.backward()  # 反向传播
             optimizer.step()  # 更新参数
 
             # ###########    可视化打印   ############
-        # print('Train Epoch: {} Loss: {:.6f} LSD: {:.6f}'.format(epoch + 1, train_loss.item(), train_lsd.item()))
-        print('Train Epoch: {}'.format(epoch + 1))
+        print('Train Epoch: {} Loss: {:.6f} LSD: {:.6f}'.format(epoch + 1, train_loss.item(), train_lsd.item()))
+        # print('Train Epoch: {}'.format(epoch + 1))
         # ###########    TensorBoard可视化 summary  ############
         # lr_schedule.step()  # 学习率衰减
         # writer.add_scalar(tag="lr", scalar_value=model.state_dict()['param_groups'][0]['lr'], global_step=epoch + 1)
         # writer.add_scalar(tag="train_loss", scalar_value=train_loss.item(), global_step=epoch + 1)
         # writer.add_scalar(tag="train_lsd", scalar_value=train_lsd.item(), global_step=epoch + 1)
-        writer.flush()
+        # writer.flush()
 
         # 神经网络在验证数据集上的表现
         model.eval()  # 测试模型
@@ -120,23 +122,25 @@ def main():
                 val_nearend_magnitude = val_nearend_magnitude.to(device)
 
                 # 前向传播
-                val_pred_mask = model(val_X)
-                val_loss = criterion(val_pred_mask, val_mask)
+                val_pred_mask = model(val_X).to(device)
 
                 # 近端语音信号频谱 = mask * 麦克风信号频谱 [batch_size, 161, 999]
-                val_pred_near_spectrum = val_pred_mask * val_nearend_mic_magnitude
-                val_lsd = pytorch_LSD(val_nearend_magnitude, val_pred_near_spectrum)
+                val_pred_near_spectrum = (val_pred_mask * val_nearend_mic_magnitude).to(device)
+                val_lsd = pytorch_LSD(val_nearend_magnitude, val_pred_near_spectrum).to(device)
 
+                val_loss = criterion(val_nearend_magnitude, val_pred_near_spectrum).to(device)
+                # val_loss = criterion(val_pred_mask, val_mask)
+                # print('train_loss:', val_loss)
             # ###########    可视化打印   ############
-            # print('  val Epoch: {} \tLoss: {:.6f}\tlsd: {:.6f}'.format(epoch + 1, val_loss.item(), val_lsd.item()))
-            print('  val Epoch: {} \t'.format(epoch + 1))
+            print('  val Epoch: {} \tLoss: {:.6f}\tlsd: {:.6f}'.format(epoch + 1, val_loss.item(), val_lsd.item()))
+            # print('  val Epoch: {} \t'.format(epoch + 1))
 
             ######################
             # 更新tensorboard    #
             ######################
             # writer.add_scalar(tag="val_loss", scalar_value=val_loss.item(), global_step=epoch + 1)
             # writer.add_scalar(tag="val_lsd", scalar_value=val_lsd.item(), global_step=epoch + 1)
-            writer.flush()
+            # writer.flush()
 
         # # ###########    保存模型   ############
         if (epoch + 1) % 10 == 0:
